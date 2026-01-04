9c2e940aa00d01131cdfad5808db32860bd9a557
Mohammad Alhabil <mohammadalhabil1999@gmail.com>
2026-01-04 14:41:32 +0200

    feat: initialize React app with Vite and Mantine
    - Add PostCSS configuration with Mantine preset and simple variables.
    - Include Vite logo SVG and React logo SVG assets.
    - Create main App component with basic counter functionality and logo links.
    - Add global styles in index.css for dark and light themes.
    - Set up main entry point with React StrictMode and MantineProvider.
    - Configure TypeScript with appropriate settings for app and node environments.
    - Set up Vite configuration with React plugin and Babel support.

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createTheme, MantineProvider } from "@mantine/core";
+import "./index.css";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </StrictMode>,
+);
