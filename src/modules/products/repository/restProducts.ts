5f33a84ea99cd7e05bab9546351d5928a028b502
Mohammad Alhabil <mohammadalhabil1999@gmail.com>
2026-01-10 14:05:31 +0200

    feat: add Product and Review types, implement ProductsRepository and restProducts for product fetching

---
+++ src/modules/products/repository/restProducts.ts
@@ -0,0 +1,16 @@
+import type { Product } from "../entities/Product";
+import type { ProductsRepository } from "./ProductsRepository";
+
+const Base_URL = "https://dummyjson.com/products";
+
+export const restProducts = (): ProductsRepository => {
+  return {
+    getAll: async (): Promise<Product[]> => {
+      const response = await fetch(Base_URL);
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      return response.json().then((data) => data.products);
+    },
+  };
+};
