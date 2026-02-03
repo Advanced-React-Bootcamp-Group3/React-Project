499a2b132d32183fb169b3b8ebb75413c135e6cc
Mohammad Alhabil <mohammadalhabil1999@gmail.com>
2026-02-03 17:53:08 +0200

    add apps/web folder + move all related files inside + create vite + tsconfig + package.json files

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    TanStackRouterVite(),
+  ],
+  resolve: {
+    preserveSymlinks: false,
+  },
+  css: {
+    postcss: "../postcss.config.cjs",
+  },
+  server: {
+    port: 3000,
+  },
+});
