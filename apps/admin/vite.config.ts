bc95d282e620e37f6bc0f131a2e36eb798865546
Mohammad Alhabil <mohammadalhabil1999@gmail.com>
2026-02-04 00:19:29 +0200

    add proxy

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname),
+  base: "/admin",
   server: {
     port: 5000,
   },
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -20,5 +20,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      "/admin": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
 });
